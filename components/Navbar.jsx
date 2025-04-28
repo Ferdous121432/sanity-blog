5fdb74b70d5d042880ac8347c7076432d328b453
Md Firdous Azam <firdousazam@iut-dhaka.edu>
2025-04-28 23:06:16 +0600

    logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ async function Navbar() {
     <div className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
-          <Image src="/logo.png" alt="Logo" width={144} height={30} />
+          <Image src="/logo.png" alt="Logo" width={30} height={30} />
         </Link>
 
         <div className="flex items-center gap-5 text-black default">
